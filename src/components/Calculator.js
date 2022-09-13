424dc52c36edc0867c7d99f7720ea34ac7d8d9bd
yasinabdmahmood <abd181103@gmail.com>
2022-09-13 14:54:13 +0300

    Refactor calculator component to use function instead of class

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,71 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './Item';
 import calculate from '../logic/calculate';
-// import operate from '../logic/operate';
-// eslint-disable-next-line react/prefer-stateless-function
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleObjectChange = this.handleObjectChange.bind(this);
-    this.state = {
-      obj: {
-        total: null,
-        next: null,
-        operation: null,
-      },
-      display: '',
-    };
-  }
 
-  handleObjectChange(btn) {
-    const { obj } = this.state;
+function Calculator() {
+  const [obj, setObj] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+  const [display, setDisplay] = useState('');
+
+  const handleObjectChange = (btn) => {
     const newObj = calculate(obj, btn);
-    this.setState(() => ({
-      obj: newObj,
-    }));
+    setObj(newObj);
+
     if (btn === '=') {
-      this.setState(() => ({
-        display: newObj.total,
-      }));
+      setDisplay(newObj.total);
     } else if (btn === 'AC') {
-      this.setState(() => ({
-        display: '',
-      }));
+      setDisplay('');
     } else {
-      this.setState((state) => ({
-        display: state.display + btn,
-      }));
+      setDisplay(display + btn);
     }
-  }
+  };
 
-  render() {
-    const { display } = this.state;
-    return (
-      <div className="calculator">
-        <div className="dark-gray">{display}</div>
-        <Item label="AC" cl="light-gray" handleObjectChange={this.handleObjectChange} />
-        <Item label="+/-" cl="light-gray" handleObjectChange={this.handleObjectChange} />
-        <Item label="%" cl="light-gray" handleObjectChange={this.handleObjectChange} />
-        <Item label="÷" cl="orange" handleObjectChange={this.handleObjectChange} />
-        <Item label="7" cl="light-gray" handleObjectChange={this.handleObjectChange} />
-        <Item label="8" cl="light-gray" handleObjectChange={this.handleObjectChange} />
-        <Item label="9" cl="light-gray" handleObjectChange={this.handleObjectChange} />
-        <Item label="x" cl="orange" handleObjectChange={this.handleObjectChange} />
-        <Item label="4" cl="light-gray" handleObjectChange={this.handleObjectChange} />
-        <Item label="5" cl="light-gray" handleObjectChange={this.handleObjectChange} />
-        <Item label="6" cl="light-gray" handleObjectChange={this.handleObjectChange} />
-        <Item label="-" cl="orange" handleObjectChange={this.handleObjectChange} />
-        <Item label="1" cl="light-gray" handleObjectChange={this.handleObjectChange} />
-        <Item label="2" cl="light-gray" handleObjectChange={this.handleObjectChange} />
-        <Item label="3" cl="light-gray" handleObjectChange={this.handleObjectChange} />
-        <Item label="+" cl="orange" handleObjectChange={this.handleObjectChange} />
-        <Item label="0" cl="double light-gray" handleObjectChange={this.handleObjectChange} />
-        <Item label="." cl="light-gray" handleObjectChange={this.handleObjectChange} />
-        <Item label="=" cl="orange" handleObjectChange={this.handleObjectChange} />
+  return (
+    <div className="calculator">
+      <div className="dark-gray">{display}</div>
+      <Item label="AC" cl="light-gray" handleObjectChange={handleObjectChange} />
+      <Item label="+/-" cl="light-gray" handleObjectChange={handleObjectChange} />
+      <Item label="%" cl="light-gray" handleObjectChange={handleObjectChange} />
+      <Item label="÷" cl="orange" handleObjectChange={handleObjectChange} />
+      <Item label="7" cl="light-gray" handleObjectChange={handleObjectChange} />
+      <Item label="8" cl="light-gray" handleObjectChange={handleObjectChange} />
+      <Item label="9" cl="light-gray" handleObjectChange={handleObjectChange} />
+      <Item label="x" cl="orange" handleObjectChange={handleObjectChange} />
+      <Item label="4" cl="light-gray" handleObjectChange={handleObjectChange} />
+      <Item label="5" cl="light-gray" handleObjectChange={handleObjectChange} />
+      <Item label="6" cl="light-gray" handleObjectChange={handleObjectChange} />
+      <Item label="-" cl="orange" handleObjectChange={handleObjectChange} />
+      <Item label="1" cl="light-gray" handleObjectChange={handleObjectChange} />
+      <Item label="2" cl="light-gray" handleObjectChange={handleObjectChange} />
+      <Item label="3" cl="light-gray" handleObjectChange={handleObjectChange} />
+      <Item label="+" cl="orange" handleObjectChange={handleObjectChange} />
+      <Item label="0" cl="double light-gray" handleObjectChange={handleObjectChange} />
+      <Item label="." cl="light-gray" handleObjectChange={handleObjectChange} />
+      <Item label="=" cl="orange" handleObjectChange={handleObjectChange} />
 
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Calculator;
