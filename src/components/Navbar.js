98f5abcd60eab1b83d51a6a96a6b0573fcfeb609
yasinabdmahmood <abd181103@gmail.com>
2022-09-21 18:45:04 +0300

    Write test for Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ const Navbar = () => (
   <div className="nav-container">
     <h1>Math Magician</h1>
     <ul className="navbar">
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/Calculator">Calculator</Link></li>
-      <li><Link to="/Quote">Quote</Link></li>
+      <li data-testid="li"><Link to="/">Home</Link></li>
+      <li data-testid="li"><Link to="/Calculator">Calculator</Link></li>
+      <li data-testid="li"><Link to="/Quote">Quote</Link></li>
     </ul>
   </div>
 );
